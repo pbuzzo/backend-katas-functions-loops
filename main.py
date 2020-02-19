16ada5f9983cc277c8d7be36b480db484022a8a9
Patrick Buzzo <pbuzzo04@gmail.com>
2020-02-19 10:18:56 -0600

    Commit Finished AAssessment Files

--- main.py
+++ main.py
@@ -1,37 +1,85 @@
 #!/usr/bin/env python
 """Implements math functions without using operators except for '+' and '-' """
+""" """
 
-__author__ = "???"
+__author__ = "Patrick Buzzo"
 
 
 def add(x, y):
-    """Add two integers. Handles negative values."""
-    # your code here
-    return
+    new_num = x + y
+    return new_num
 
 
 def multiply(x, y):
-    """Multiply x with y. Handles negative values of x or y."""
-    # your code here
-    return
+    new_num = 0
+    if y >= 0:
+        for index in range(y):
+            new_num += x
+    else:
+        for index in range(-y):
+            new_num -= x
+    return new_num
+    # new_num = 0
+    # count = 0
+    # if (x < 0) and (y >= 0):
+    #     while count < y:
+    #         new_num = new_num + x
+    #         count += 1
+    # elif (x >= 0) and (y < 0):
+    #     y = -y
+    #     while count < y:
+    #         new_num = new_num + x
+    #         count += 1
+    # elif (x >= 0) and (y >= 0):
+    #     while count < y:
+    #         new_num = new_num + x
+    #         count += 1
+    # elif (x < 0) and (y < 0):
+    #     x = -x
+    #     y = -y
+    #     while count < y:
+    #         new_num = new_num + x
+    #         count += 1
+    # return new_num
 
 
 def power(x, n):
     """Raise x to power n, where n >= 0"""
-    # your code here
-    return
+    if n == 0:
+        return 1
+    elif x == 0:
+        return 0
+    else:
+        new_num = x
+        if n == 1:
+            return x
+        else:
+            for index in range(n-1):
+                new_num = multiply(new_num, x)
+    return new_num
 
 
 def factorial(x):
     """Compute factorial of x, where x > 0"""
-    # your code here
-    return
+    new_num = x
+    if x == 0 or x == 1:
+        return 1
+    if x > 1:
+        count = x - 1
+        while count > 0:
+            new_num = multiply(new_num, count)
+            count -= 1
+    return new_num
 
 
 def fibonacci(n):
-    """Compute the nth term of fibonacci sequence"""
-    # your code here
-    return
+    """Compute the nth term of fibonacci sequence
+    Resources: http://bit.ly/2P5asH2 , http://bit.ly/2vOJDji
+    """
+    a, b = 0, 1
+    for index in range(n):
+        a, b = b, a + b
+    return a
 
 
 if __name__ == '__main__':
